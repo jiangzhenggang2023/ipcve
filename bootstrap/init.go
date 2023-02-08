74cd281c04c7e98122ece133bc1beff245ceb8ea
Jiang <zhenggang.jiang@intel.com>
2023-02-08 20:54:30 +0800

    commit

--- bootstrap/init.go
+++ bootstrap/init.go
@@ -1,1 +1,20 @@
 package bootstrap
+
+import (
+	my_errors "ipcve/app/utils/errors"
+	"log"
+	"os"
+)
+
+func checkRequiredFolders() {
+	//1.检查配置文件是否存在
+	if _, err := os.Stat("./config/config.yml"); err != nil {
+		log.Fatal(my_errors.ErrorsConfigYamlNotExists + err.Error())
+	}
+}
+
+func init() {
+	// 检查文件是否存在，不存在则创建
+	checkRequiredFolders()
+	log.Println("Test.....")
+}
